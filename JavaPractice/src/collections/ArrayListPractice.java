99c761c7e2544fe272d133cc4206b026796aa86f
moses <moses@moses-comp>
2021-01-27 10:12:13 -0500

    second commit

--- JavaPractice/src/collections/ArrayListPractice.java
+++ JavaPractice/src/collections/ArrayListPractice.java
@@ -1,9 +1,17 @@
 package collections;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ArrayListPractice {
 	
 	
 	public static void main(String[] args) {
+		
+		
+		ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(1,1,2,2,3,3));
+		
+		//1. LinkedHashSet
 		
 	}
 
